251efb6e5ea655eeeca13cae7bbccaedef9a25a6
Satish <satishprasad@logisticinfotech.co.in>
2019-03-26 18:01:10 +0530

    Initial Commit

---
+++ mylibrary/src/main/java/com/rn/mylibrary/Calculator.java
@@ -0,0 +1,21 @@
+package com.rn.mylibrary;
+
+public class Calculator {
+
+    public static int addition(int i,int j){
+        return i+j;
+    }
+
+    public static int subtraction(int i,int j){
+        return i-j;
+    }
+
+    public static int multiplication(int i,int j){
+        return i*j;
+    }
+
+    public static double division(int i,int j){
+        return i/j;
+    }
+
+}
